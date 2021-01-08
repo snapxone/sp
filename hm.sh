4d557ba75e8d8033164ca51bd33306736b68228c
snapxone <70817534+snapxone@users.noreply.github.com>
2021-01-08 20:17:28 +0500

    Create hm.sh

---
+++ hm.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm install -g typescript
+
+git clone https://github.com/arghyac35/aria-telegram-mirror-bot.git && cd aria-telegram-mirror-bot
+
+npm install --legacy-peer-deps
+
+npm audit fix
