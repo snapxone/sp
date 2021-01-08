09b397faabf4ada37e410a3d3c71a67d72eeb7ed
snapxone <70817534+snapxone@users.noreply.github.com>
2021-01-08 20:48:34 +0500

    Update hm.sh

--- hm.sh
+++ hm.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#Updating & Upgrading
+#Updating & Upgrading System
 
 sudo apt-get update
 sudo apt-get upgrade
 
+#Required Packages for Bot
+
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
-
-#Pre Stuff
-
 sudo apt install aria2
-sudo npm install -g typescript
 
-#After Stuff
+sudo npm install -g typescript
 
 git clone https://github.com/arghyac35/aria-telegram-mirror-bot.git && cd aria-telegram-mirror-bot
 npm install --legacy-peer-deps
